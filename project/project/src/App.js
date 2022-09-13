58211731b1060b5f51b070ee6e719d90d9bdb985
jwc <612346@naver.com>
2022-09-13 17:05:37 +0900

    0913

--- project/project/src/App.js
+++ project/project/src/App.js
@@ -1,27 +1,82 @@
 // import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Nav(props) {
+  let lis = [];
+
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+    <a href={'/read/' + t.id} id={t.id} onClick={(e) => {
+      e.preventDefault();
+      props.onChangeMode(Number(e.target.id));
+    }
+  }>{t.title}</a></li>);
+  }
+
+  return <header>
+  <ul>
+    {lis.unshift(<div key={}></div>)}
+    {lis}
+  </ul>
+</header>
+}
+
+function Article(props) {
+  return <article>
+    <h1>{props.title}</h1>
+    {props.body}
+  </article>
+}
 
 function App() {
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
+  const topics = [
+    {id:1, title:"ABOUT"},
+    {id:2, title:"SKILLS"},
+    {id:3, title:"PROJECT"},
+    {id:4, title:"CONTACT"}
+  ];
+
+  let content = null;
+
+  if (mode === 'WELCOME') {
+    content = <Article title="WELCOME" body="hello, WEB"></Article>
+  } else if (mode === 'READ') {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+
   return (
     
     <div>
-      <header>
-        <ul>
-          <li>ABOUT</li>
-          <li>SKILLS</li>
-          <li>PROJECT</li>
-          <li>CONTACT</li>
-        </ul>
-      </header>
+      
+      <Nav logo="MY PORTFOLIO" topics={topics} onChangeMode={(id) => {
+        alert(id);
+        setMode('READ');
+        setId();
+      }}></Nav>
 
-      <nav>
-        <ul>
-          <li>ABOUT</li>
-          <li>SKILLS</li>
-          <li>PROJECT</li>
-          <li>CONTACT</li>
-        </ul>
-      </nav>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        alert(id);
+        setMode('WELCOME');
+        setId(_id);
+      }}></Nav>
+
+      {content}
     </div>
   );
 }
